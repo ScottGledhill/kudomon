31d0e33bf79264feb90486db4bc04133a40df242
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-03 13:08:03 +0000

    grass module made

--- lib/kudomon_types/fire.rb
+++ lib/kudomon_types/fire.rb
@@ -1,4 +1,4 @@
-module Fires
+module Fire
 
   {
    'Mancharred' => {
---
+++ lib/kudomon_types/grass.rb
@@ -0,0 +1,14 @@
+module Grass
+
+  {
+   'Sourbulb' => {
+     :HP => 100,
+     :CP => 20
+   },
+
+   'Execute' => {
+     :HP => 200,
+     :CP => 45
+   }
+  }
+end
