b1924fecb1931f1e5c29c08e13620da5974695fc
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-03 13:18:51 +0000

    one kudomon added

--- lib/kudomon.rb
+++ lib/kudomon.rb
@@ -1,7 +1,12 @@
+require 'kudomon_types/electric'
+
 class Kudomon
-  attr_reader :coordinates
+  attr_reader :coordinates, :kudomon
+  include Electric
+
   def initialize
     @coordinates = [0,0]
+    @kudomon = [Electric]
   end
   # def move
   # end
